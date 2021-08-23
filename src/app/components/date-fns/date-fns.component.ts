40b84031c647a37828daac8299eba4bc8f4dfb19
namnd <dinhnambk96@gmail.com>
2021-08-23 14:08:17 +0700

    import date fns

--- src/app/components/date-fns/date-fns.component.ts
+++ src/app/components/date-fns/date-fns.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { compareAsc, format } from 'date-fns'
 
 @Component({
   selector: 'app-date-fns',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-fns.component.scss']
 })
 export class DateFnsComponent implements OnInit {
+  dates = [
+    new Date(1995, 6, 2),
+    new Date(1987, 1, 11),
+    new Date(1989, 6, 10),
+  ]
+
+  dateFormated = format(new Date(2014, 1, 11), 'yyyy-MM-dd');
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.dates.sort(compareAsc));
+
   }
 
 }
